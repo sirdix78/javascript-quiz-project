dc501e332b6a7d25df957934eaa271b61c0bdf63
palaci spaci <kristi.gjoka92@gmail.com>
2025-02-24 17:25:12 +0100

    done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,38 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining; 
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
+   getQuestion() {
+      return this.questions[this.currentQuestionIndex];
+   }
+
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
+
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1; i > 0; i--){
+            let j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+        return this.shuffleQuestions;
+    } 
     
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+    checkAnswer(answer) {
+        if (answer) {
+            this.correctAnswers++;
+        }  
+    }
+    hasEnded() {
+        if(this.currentQuestionIndex < this.questions.length){
+            return false;
+        } else if (this.currentQuestionIndex === this.questions.length) {
+            return true;
+    }
+}
 }
